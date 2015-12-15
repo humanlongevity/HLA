29919254b422e2f10e7068abef2b024ae04fbed2
xchao-hli <xchao@humanlongevity.com>
2015-12-15 16:34:13 +0800

    working. need to work on the N type and zygosity

--- bin/by.msa.pl
+++ bin/by.msa.pl
@@ -1,132 +1,219 @@
 #!/usr/bin/env perl
 use strict;
+use List::Util qw(min);
 
-die "usage: $0 msa daa\n" unless $#ARGV == 1;
+die "usage: $0 fastq msa daa\n" unless $#ARGV == 2;
+my $fastq_file = shift;
 my $msa_file = shift;
 my $daa_file = shift;
 
+print STDERR "processing FASTQ file\n";
+my %qseq;
+my %qlen;
+open(IN, $fastq_file) or die $!;
+while(my $h = <IN>)
+{
+	my $seq = <IN>;
+	my $temp = <IN>;
+	my $temp = <IN>;
+	$h =~ s/^\@(\S+).+/\1/s;
+	chomp $seq;
+	$qseq{$h} = $seq;
+	$qlen{$h} = length $seq;
+}
+print STDERR "found ", scalar(keys %qseq), " reads";
+
 print STDERR "processing MSA file\n";
-my %seq;
-my %start;
-my %end;
+my %tseq;
+my %tlen;
+my %pre;
+my %suf;
+my %type;
+my %gene;
+my %is_gene;
 open(IN, $msa_file) or die $!;
 while(<IN>)
 {
 	chomp;
-	my ($id, $size, $from, $to, $diff, $seq) = split(/\t/, $_);
-	$seq =~ s/\./-/g;
-	$seq =~ s/\*/./g;
-	$seq{$id} = $seq;
-	$start{$id} = $from;
-	$end{$id} = $to;
+	my ($id, $type, $exon, $size, $full, $pre, $suf, $suf2, $seq) = split(/\t/, $_);
+	$tseq{$id} = $seq;
+	$type{$id} = $type;
+	$pre{$id} = $pre;
+	$suf{$id} = $suf;
+	$tlen{$id} = $seq=~s/[^-]//g;
+	my $g = $1 if $type =~ m/^(\S+)\*/;
+	$g = 'ClassI' if $g =~ m/^[ABC]$/;
+	$gene{"$g\t$exon"}->{$id} = 1;
+	$is_gene{$id} = "$g\t$exon";
 }
-print STDERR "found ", scalar(keys %seq), " HLA types\n";
+print STDERR "found ", scalar(keys %tseq), " HLA exons\n";
 
 print STDERR "processing DAA file\n";
-open(IN, "diamond view -a '$daa_file' -f sam -o /dev/stdout |") or die $!;
+open(IN, "diamond view -a '$daa_file' -o /dev/stdout |") or die $!;
+my %mlen;
 my %match;
 my %matched;
-my %mpos;
-my %mseq;
-my %mlen;
 my %nonspec;
 while(<IN>)
 {
-	next if m/^@/;
-	my @f = split(/\t/, $_);
-	next unless $f[16] =~ m/:100$/;
-	my $q = $f[0];
-	my $seq = $f[9];
-	my $len = length $seq;
-	if($len > $mlen{$q})
+	my ($q, $target, $identity, $len, $mis, $gap, $qs, $qe, $ts, $te, $e, $score) = split(/\t/, $_);
+	next unless $identity == 100;
+	my $len = $te - $ts + 1;
+	my $g = $is_gene{$target};
+	if($len > $mlen{$g}->{$q})
 	{
-		$mlen{$q} = $len;
-		$mseq{$q} = $seq;
-		delete $match{$q} if $match{$q};
+		$mlen{$g}->{$q} = $len;
+		$match{$g}->{$q} = [$target, $qs, $qe, $ts, $te];
+		print STDERR "\t$target\n" unless $matched{$target};
 	}
-	my $t = $f[2];
-	if(not(exists $start{$t}))
-	{
-		$nonspec{$q}++;
-	}else
-	{
-		$matched{$t}++;
-		if(not $match{$q})
-		{
-			$match{$q} = $t;
-			$mpos{$q} = $start{$t} + $f[3] - 1;
-		}
-	}
+	$matched{$target}++ if $len >= $mlen{$g}->{$q};
+	$nonspec{$q}++ if not($tseq{$target}) && $len >= $mlen{$g}->{$q};
 }
-print STDERR scalar(keys %match), " reads matched to ", scalar(keys %matched), " HLA types\n";
+print STDERR "matched to ", scalar(keys %matched), " HLA exons\n";
 print STDERR scalar(keys %nonspec), " reads matched to HLA types not in the MSA file\n";
 
 print STDERR "translating matches to MSA\n";
-for my $q(keys %match)
+my %done;
+for my $g(keys %match)
 {
-	my $t = $match{$q};
-	my $upstream = substr($seq{$t}, 0, $mpos{$q});
-	my $upinsert = $upstream=~s/-/-/g;
-	my $start = $upinsert + $mpos{$q};
-	if(substr($seq{$t}, $start) =~ m/^(-+)/)
+	print STDERR "\t$g\n";
+	for my $q(keys %{$match{$g}})
 	{
-		$start += $+[1] + 1;
-	}
-	my @ref = split(//, substr($seq{$t}, $start));
-	my @seq = split(//, $mseq{$q});
-	my $seq;
-	my $qseq;
-	while(my $rs = shift @ref)
-	{
-		last unless @seq;
-		if($rs eq '-')
-		{
-			$seq .= $rs;
-			$qseq .= $rs;
-		}else
-		{
-			my $qs = shift @seq;
-			$seq .= $rs;
-			$qseq .= $qs;
-		}
-	}
-	print STDERR "DIFFERENT: $q on $t at $mpos{$q} (=> $start):\nQ: $qseq\nT: $seq\nQuery: $mseq{$q}\nTARG-up: $upstream\nTARG: $seq{$t}\n\n" if $seq ne $qseq;
-	$mseq{$q} = $qseq;
-	$mlen{$q} = length $qseq;
-	$mpos{$q} = $start;
-}
-print STDERR "done translating\n";
+		print STDERR "\t\t$q\n";
+		my ($target, $qs, $qe, $ts, $te) = @{$match{$g}->{$q}};
+		my $spe = $nonspec{$q} ? 0 : 1;
+		print STDERR "\t\t\t$q($qlen{$q}):$qs-$qe vs $target($tlen{$target}):$ts-$te\n";
 
-print STDERR "building match matrix\n";
-my $spec = 0;
-my $nspec = 0;
-for my $q(keys %match)
-{
-	my $flag = $nonspec{$q} ? 0 : 1;
-	if($flag)
-	{
-		$spec++;
-	}else
-	{
-		$nspec++;
-	}
-	for my $t(keys %matched)
-	{
-		my $tseq = substr($seq{$t}, $mpos{$q}, $mlen{$q});
-		my $good;
-		if($mseq{$q} eq $tseq)
+		my $qseq = $qseq{$q};
+		if($qs > $qe)
 		{
-			$good = $mlen{$q};
-		}else
+			$qseq =~ tr/ATGC/TACG/;
+			$qseq = reverse $qseq;
+			print STDERR "\t\t\t\tflipping $qs-$qe to";
+			$qs = $qlen{$q} - $qs + 1;
+			$qe = $qlen{$q} - $qe + 1;
+			print STDERR " $qs-$qe\n";
+		}
+		my $mseq = translate($qseq, $qs, $qe);
+
+		my ($pre1, $pre2, $suf1, $suf2);
+		if($ts == 1)
 		{
-			$tseq =~ s/\.//g;
-			my $tlen = length($tseq);
-			if($tlen && $mseq{$q} =~ m/$tseq/)
+			my $p1 = $qs-2;
+			my $p2 = $qs-3;
+			$pre1 = substr($qseq, $p1, 1) if $p1 >= 0;
+			$pre2 = substr($qseq, $p2, 2) if $p2 >= 0;
+		}
+		if($te == $tlen{$target})
+		{
+			$suf1 = substr($qseq, $qe, 1);
+			$suf2 = substr($qseq, $qe, 2);
+		}
+		print STDERR "\t\t\t\tchecking prefix or suffix leftover on the query:\n";
+		print STDERR "\t\t\t\t\tpre1=$pre1, pre2=$pre2, suf1=$suf1, suf2=$suf2\n";
+
+		my $tag = "$g-$pre2-$pre1-$mseq-$suf1-$suf2";
+		print STDERR "\t\t\t\ttag code: $tag\n";
+		next if $done{$tag};
+		$done{$tag} = 1;
+		print STDERR "\t\t\t\ttag not processed yet\n";
+
+		my $left = min(int(($qs-1)/3), $ts-1);
+		my $right = min(int(($qlen{$q}-$qe)/3), $tlen{$target} - $te);
+
+		my $start = 0;
+		my $end = 0;
+		my $N = 0;
+		my $n = 0;
+		my @aa = split(//, $tseq{$target});
+		for my $a(@aa)
+		{
+			$n++;
+			$N++ unless $a eq '-';
+			$start = $n if $N == $ts && not($start);
+			if($N == $te)
 			{
-				$good = $tlen;
+				$end = $n;
+				last;
 			}
 		}
-		print "$q\t$t\t$mpos{$q}\t$mlen{$q}\t$good\t$flag\t$mseq{$q}\n" if $good;
+		my $tlen = $end - $start + 1;
+		$start--;
+		my $tseq = substr($tseq{$target}, $start, $tlen);
+		print STDERR "\t\t\t\tconverting $ts-$te position to MSA $start-$end\n";
+
+				
+		# the sequence check part should be removed later. Only for debuging purpose
+		#my $tseq2 = $tseq;
+		#$tseq2 =~ s/-//g;
+		#die "DIFFERENT: $q ($qs-$qe: $mseq) with $target ($start-$end: $tseq):\n$qseq$q}\n$tseq{$target}\n\n" if $mseq ne $tseq2;
+
+		for my $t(keys %{$gene{$g}})
+		{
+			print STDERR "\t\t\t\t\tcomparing with $t\n";
+			next unless $matched{$t};
+			print STDERR "\t\t\t\t\tsaw this before ($matched{$t} times)\n";
+			my $comp = substr($tseq{$t}, $start, $tlen);
+			next unless $comp eq $tseq;
+			print STDERR "\t\t\t\t\tsequence matched\n";
+			my $pre_len = length $pre{$t};
+			my ($pre, $suf);
+			print STDERR "\t\t\t\t\tchecking prefix match\n" if $pre_len;
+			if($pre_len == 1)
+			{
+				next if $pre1 && $pre{$t} ne $pre1;
+				$pre = $pre1;
+			}elsif($pre_len == 2)
+			{
+				next if $pre2 && $pre{$t} ne $pre2;
+				$pre = $pre2;
+			}
+			print STDERR "\t\t\t\t\tprefix matched ($pre{$t} vs $pre)\n" if $pre_len;
+			my $suf_len = length $suf{$t};
+			print STDERR "\t\t\t\t\tchecking suffix match\n" if $suf_len;
+			if($suf_len == 1)
+			{
+				next if $suf1 && $suf{$t} ne $suf1;
+				$suf = $suf1;
+			}elsif($suf_len == 2)
+			{
+				next if $suf2 && $suf{$t} ne $suf2;
+				$suf = $suf2;
+			}
+			print STDERR "\t\t\t\t\tsuffix matched ($suf{$t} vs $suf)\n" if $pre_len;
+			print "$q\t$qlen{$q}:$qs-$qe\t$t\t$tlen{$t}:$ts-$te\t$type{$t}\t$is_gene{$t}\t$spe\t$left\t$right\t$start\t$end\n";
+		}
 	}
 }
-print STDERR "wrote $spec specific reads and $nspec non-specific reads\n";
+
+sub translate
+{
+	my ($seq, $from, $to) = @_;
+	my %codon = (
+  	TTT => "F", TTC => "F", TTA => "L", TTG => "L",
+  	TCT => "S", TCC => "S", TCA => "S", TCG => "S",
+  	TAT => "Y", TAC => "Y", TAA => "X", TAG => "X",
+  	TGT => "C", TGC => "C", TGA => "X", TGG => "W",
+  	CTT => "L", CTC => "L", CTA => "L", CTG => "L",
+  	CCT => "P", CCC => "P", CCA => "P", CCG => "P",
+  	CAT => "H", CAC => "H", CAA => "Q", CAG => "Q",
+  	CGT => "R", CGC => "R", CGA => "R", CGG => "R",
+  	ATT => "I", ATC => "I", ATA => "I", ATG => "M",
+  	ACT => "T", ACC => "T", ACA => "T", ACG => "T",
+  	AAT => "N", AAC => "N", AAA => "K", AAG => "K",
+  	AGT => "S", AGC => "S", AGA => "R", AGG => "R",
+  	GTT => "V", GTC => "V", GTA => "V", GTG => "V",
+  	GCT => "A", GCC => "A", GCA => "A", GCG => "A",
+  	GAT => "D", GAC => "D", GAA => "E", GAG => "E",
+  	GGT => "G", GGC => "G", GGA => "G", GGG => "G",
+	);
+	my $seq2 = substr($seq, $from - 1, $to - $from + 1);
+	if($from > $to)
+	{
+		$seq2 = substr($seq, $to - 1, $from - $to + 1);
+		$seq2 =~ tr/ATGC/TACG/;
+		$seq2 = reverse $seq2;
+	}
+	$seq2 =~ s/(...)/$codon{$1}/ge;
+	return $seq2;
+}
---
+++ bin/typing.r
@@ -0,0 +1,74 @@
+#!/usr/bin/env Rscript
+args <- commandArgs(T)
+library(parallel)
+options(mc.cores = detectCores())
+library(data.table)
+
+dt <- fread(args[1])
+setnames(dt, c('q', 'qpos', 't', 'tpos', 'type', 'msa', 'exon', 'specific', 'left', 'right', 'start', 'end'))
+
+# filter pair end matches
+dt[, qp := sub('/\\d$', '', q)]
+nr <- dt[, .(pair1 = length(unique(q))), keyby = qp]
+setkey(dt, qp)
+dt <- nr[dt]
+nr <- dt[, .(pair2 = length(unique(q))), keyby = .(qp, t)]
+setkey(dt, qp, t)
+dt <- nr[dt]
+dt <- dt[pair1 == pair2]
+
+# filter non-specific matching
+dt <- dt[specific==1 & left==0 & right==0]
+# TODO, filter core exons
+
+#library(IRanges)
+#setkey(dt, t)
+#cov <- dt[, .(
+#	n = .N, 
+#	cov = sum(width(reduce(IRanges(pos, width = len)))),
+#), keyby = t]
+
+mat <- dcast(dt, q ~ type, value.var = 'specific', fun.aggregate = max, fill = 0)
+qs <- mat$q
+mat[, q := NULL]
+allele.names <- colnames(mat)
+allele.genes <- unique(sub('\\*.+', '', allele.names))
+alleles <- 1:ncol(mat)
+reads <- 1:nrow(mat)
+na <- length(alleles)
+nr <- length(reads)
+
+library(lpSolve)
+f.obj <- c(rep(0, na), rep(1, nr))
+
+# constraints for hit incidence matrix
+f.con <- do.call(rbind, mclapply(1:nr, function(i){
+    con <- c(mat[i,], rep(0, nr))
+    con[na + i] <- -1
+    con
+}))
+f.dir <- rep('>=', nr)
+f.rhs <- rep(0, nr)
+
+# constraints for 1 or 2 alleles per gene
+f.con2 <- do.call(rbind, mclapply(allele.genes, function(gene){
+    con <- c(rep(0, na), rep(0, nr))
+    con[grep(sprintf("^%s", gene), allele.names)] <- 1
+    rbind(con, con)
+}))
+f.dir2 <- rep(c('>=', '<='), length(allele.genes))
+f.rhs2 <- rep(c( 1,    2  ), length(allele.genes))
+
+# final constraints
+f.con <- rbind(f.con, f.con2)
+f.dir <- c(f.dir, f.dir2)
+f.rhs <- c(f.rhs, f.rhs2)
+
+lps <- lp('max', f.obj, f.con, f.dir, f.rhs, all.bin = T)
+solution <- lps$solution[alleles]
+names(solution) <- allele.names
+solution <- solution[order(-solution)]
+solution <- solution[solution > 0]
+print(solution)
+
+save.image(file = 'temp.rda')
