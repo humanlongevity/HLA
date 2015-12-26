aff4b5b10eb8d3d3c14ec166918d5dec6820f701
xchao-hli <xchao@humanlongevity.com>
2015-12-27 01:16:24 +0800

    more sensitive DIAMOND parameter

--- bin/typer.sh
+++ bin/typer.sh
@@ -4,7 +4,8 @@ OUT=$2
 
 samtools view -u $S3 chr6:29723339-33089696 | samtools view -u -L data/hg38.hla_region.refGene.bed - | bamtools convert -format fastq | sga preprocess -q20 -f5 -m 100 - > $OUT.fq
 #diamond blastx -C 10000 --top 10 -s 1 -c 1 -v -d data/hla -q $OUT.fq -a $OUT.daa 
-diamond blastx -C 10000 --seg no --min-score 30 --top 3 -s 1 -c 1 -v -d data/hla -q $OUT.fq -a $OUT.daa
+#diamond blastx -C 10000 --seg no --min-score 30 --top 3 -s 1 -c 1 -v -d data/hla -q $OUT.fq -a $OUT.daa
+diamond blastx -C 20000 --index-mode 2 --seg no --min-score 10 --top 5 -c 1 -v -d data/hla -q $OUT.fq -a $OUT.daa
 ./bin/by.msa.pl $OUT.fq data/hla.tsv $OUT.daa 2>$OUT.log >$OUT.tsv
 ./bin/typing.r $OUT.tsv $OUT.hla
 cat $OUT.hla
