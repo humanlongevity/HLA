3e5d255a205d5c261a8a9c1382b678d19a216cba
xchao-hli <xchao@humanlongevity.com>
2015-12-29 19:29:29 +0800

    more refined range

--- bin/typer.sh
+++ bin/typer.sh
@@ -5,7 +5,8 @@ OUT=$2
 set -ex
 
 #samtools view -u $S3 chr6:29723339-33089696 | samtools view -u -L data/hg38.hla_region.refGene.bed - | bamtools convert -format fastq | sga preprocess -q20 -f5 -m 100 - > $OUT.fq
-samtools view -u $S3 chr6:26085440-33091696 | samtools view -u -L data/hla.bed - | bamtools convert -format fastq | sga preprocess -q20 -f5 -m 100 - > $OUT.fq
+samtools view -u $S3 chr6:29886751-33090696 | samtools view -u -L data/hla.bed - > $OUT.bam
+bamtools convert -format fastq -in $OUT.bam | sga preprocess -q20 -f5 -m 100 - > $OUT.fq
 #diamond blastx -C 10000 --top 10 -s 1 -c 1 -v -d data/hla -q $OUT.fq -a $OUT.daa 
 #diamond blastx -C 10000 --seg no --min-score 30 --top 3 -s 1 -c 1 -v -d data/hla -q $OUT.fq -a $OUT.daa
 diamond blastx -C 20000 --index-mode 2 --seg no --min-score 10 --top 5 -c 1 -v -d data/hla -q $OUT.fq -a $OUT.daa
--- data/script/batch.sh
+++ data/script/batch.sh
@@ -6,3 +6,5 @@ ls exon/*.faa | perl -pe 's|.+/||; s/_nuc.faa//' | parallel ./script/006.split.p
 #./script/006.split.pl nuc/DQB_nuc.txt temp/DQB align/DQB.tsv
 #./script/006.split.pl nuc/DRB_nuc.txt temp/DRB align/DRB.tsv
 #./script/006.split.pl nuc/classI_nuc.txt temp/classI align/classI.tsv
+
+perl -ne 'print unless m/^#/' ./script/hg38.hla_region-IMGT.bed | perl -lane '$F[1]-=1000; $F[2]+=1000; print join("\t", @F)' > hla.bed
