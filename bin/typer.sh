8737f7138f669675c4eb597cce9644688db9d3ee
xchao-hli <xchao@humanlongevity.com>
2015-12-30 15:46:54 +0800

    --top 5 was too stringent, will miss some true matches, changing to 20

--- bin/typer.sh
+++ bin/typer.sh
@@ -9,7 +9,7 @@ samtools view -u $S3 chr6:29886751-33090696 | samtools view -u -L data/hla.bed -
 bamtools convert -format fastq -in $OUT.bam | sga preprocess -q20 -f5 -m 100 - > $OUT.fq
 #diamond blastx -C 10000 --top 10 -s 1 -c 1 -v -d data/hla -q $OUT.fq -a $OUT.daa 
 #diamond blastx -C 10000 --seg no --min-score 30 --top 3 -s 1 -c 1 -v -d data/hla -q $OUT.fq -a $OUT.daa
-diamond blastx -C 20000 --index-mode 2 --seg no --min-score 10 --top 5 -c 1 -v -d data/hla -q $OUT.fq -a $OUT.daa
+diamond blastx -C 20000 --index-mode 2 --seg no --min-score 10 --top 20 -c 1 -v -d data/hla -q $OUT.fq -a $OUT.daa
 ./bin/by.msa.pl $OUT.fq data/hla.tsv $OUT.daa 2>$OUT.log >$OUT.tsv
 ./bin/typing.r $OUT.tsv $OUT.hla
 cat $OUT.hla
