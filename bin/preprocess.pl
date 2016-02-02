0ba6d99513998e4e8da74c9847a4aedaef46f2d6
xchao-hli <xchao@humanlongevity.com>
2016-02-02 15:15:43 +0800

    quality trimming improvements

--- bin/preprocess.pl
+++ bin/preprocess.pl
@@ -11,9 +11,9 @@ while(<>)
 	my $name = $fields[0];
 	my $flag = $fields[1];
 	my $seq = $fields[9];
-	next unless $seq =~ m/^[ATGC]+$/;
 	my $qual = $fields[10];
 
+	# reverse complement if the read was flipped in the BAM
 	if($flag & 16){
 		$qual = reverse $qual;
 		$seq = reverse $seq;
@@ -21,41 +21,52 @@ while(<>)
 	}
 	my @qual = map { ord($_) - 33 } split('', $qual);
 
-	if($qual[$#qual] < $Q){
-		my @qual2 = @qual;
+	# remove the N-prefix
+	if($seq =~ m/^(N+)/){
+		my $n = length $1;
+		$seq = substr($seq, $n);
+		@qual = @qual[$n..$#qual];
+	}
 
-		my @trim;
-		my $i = $#qual + 1;
-		my $cum = 0;
-		my $min = 100;
-		my $which_min;
-		while(my $q = pop @qual)
+	my @qual2 = @qual;
+
+	# 3' trimming by quality
+	my @trim;
+	my $i = $#qual + 1;
+	my $cum = 0;
+	my $min = 100;
+	my $which_min = $#qual + 100;
+	while(my $q = pop @qual)
+	{
+		$i--;
+		$cum += $q - $Q;
+		unshift @trim, $cum;
+		if($cum < $min)
 		{
-			$i--;
-			$cum += $q - $Q;
-			unshift @trim, $cum;
-			if($cum < $min)
-			{
-				$min = $cum;
-				$which_min = $i;
-			}
-		}
-		if($which_min){
-			$seq = substr($seq, 0, $which_min);
-			$qual = substr($qual, 0, $which_min);
-			@qual = @qual2[0..($which_min-1)]
-		}else{
-			$seq = '';
+			$min = $cum;
+			$which_min = $i;
 		}
 	}
+	if($qual2[$which_min] < $Q){
+		$seq = substr($seq, 0, $which_min);
+		$qual = substr($qual, 0, $which_min);
+		@qual = @qual2[0..($which_min-1)]
+	}
+
+	# min length after trimming
 	next if length($seq) < $M;
 
+	# max bad bases
 	my $fail_count;
 	for my $q(@qual){
 		$fail_count++ if $q <= 3;
 	}
 	next if $fail_count > $F;
 
+	# discard reads if contains N
+	next unless $seq =~ m/^[ATGC]+$/;
+
+	# name reads with /1 or /2
 	if($flag & 128){
 		$name .= '/2';
 	}else{
