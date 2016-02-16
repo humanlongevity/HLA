9f87589ea322ac6bf18e0077a0c9d4be253ba335
xchao-hli <xchao@humanlongevity.com>
2016-02-16 14:49:56 +0800

    docker image

---
+++ docker/hla.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ex
+
+DOCKER=$1
+BAM_FILE=$2
+SAMPLE_ID=$3
+
+KEY=`perl -ne 'print $1 if m/aws_access_key_id = (\\S+)/' ~/.aws/credentials`
+SECRET=`perl -ne 'print $1 if m/aws_secret_access_key = (\\S+)/' ~/.aws/credentials`
+
+docker run --rm -u `id -u`:`id -g` -v `pwd`:/work -w /work \
+	-e AWS_ACCESS_KEY_ID=$KEY \
+	-e AWS_SECRET_ACCESS_KEY=$SECRET \
+    $DOCKER typer.sh $BAM_FILE $SAMPLE_ID
