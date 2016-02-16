79738ff3f86702dd25671f61c11a402366f16138
xchao-hli <xchao@humanlongevity.com>
2016-02-16 14:49:36 +0800

    use pwd for temp directory, instead of /dev/shm, a bit slower but easier to use in docker

--- bin/typer.sh
+++ bin/typer.sh
@@ -16,7 +16,7 @@ mkdir -p $OUT
 echo "Extracting reads from S3"
 samtools view -u $S3 chr6:29886751-33090696 | samtools view -L $BIN/../data/hla.bed - | $BIN/preprocess.pl > $OUT/$ID.fq
 echo "Aligning reads to IMGT database"
-diamond blastx -C 20000 --index-mode 2 --seg no --min-score 10 --top 20 -c 1 -v -d $BIN/../data/hla -q $OUT/$ID.fq -a $OUT/$ID.daa
+diamond blastx -t . -C 20000 --index-mode 2 --seg no --min-score 10 --top 20 -c 1 -v -d $BIN/../data/hla -q $OUT/$ID.fq -a $OUT/$ID.daa
 echo "Preparing data for the typing algorithm"
 $BIN/by.msa.pl $OUT/$ID.fq $OUT/$ID.daa $OUT/$ID.tsv
 echo "Typing"
