32b894ef629b7266f4ef773dbc0f9d7aab4db812
xchao-hli <xchao@humanlongevity.com>
2016-04-27 14:34:04 +0800

    better docker caller

--- docker/hla.sh
+++ docker/hla.sh
@@ -5,8 +5,8 @@ DOCKER=$1
 BAM_FILE=$2
 SAMPLE_ID=$3
 
-KEY=`perl -ne 'print $1 if m/aws_access_key_id = (\\S+)/' ~/.aws/credentials`
-SECRET=`perl -ne 'print $1 if m/aws_secret_access_key = (\\S+)/' ~/.aws/credentials`
+KEY=`head -n 3 ~/.aws/credentials | perl -ne 'print $1 if m/aws_access_key_id = (\\S+)/'`
+SECRET=`head -n 3 ~/.aws/credentials | perl -ne 'print $1 if m/aws_secret_access_key = (\\S+)/'`
 
 docker run --rm -u `id -u`:`id -g` -v `pwd`:/work -w /work \
 	-e AWS_ACCESS_KEY_ID=$KEY \
