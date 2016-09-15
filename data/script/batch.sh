ce2d37f2f7ba1d164af915da8ab1fc14bb52bda8
xchao-hli <xchao@humanlongevity.com>
2016-09-15 13:56:44 +0800

    changed docker version to "0.0.0" to avoid overwriting my 1.0.0

--- data/script/batch.sh
+++ data/script/batch.sh
@@ -2,9 +2,8 @@ ls ../raw/alignments/*_nuc.txt | parallel './script/000.alignment.nuc.pl {} > nu
 rm nuc/[ABC]_*
 ls nuc | parallel ./script/005.exon.pl nuc/{} exon/{.}.shift exon/{.}.faa dna/{.}.fna
 ls exon/*.faa | perl -pe 's|.+/||; s/_nuc.faa//' | parallel ./script/006.split.pl exon/{}_nuc.faa temp/{} align/{}.tsv
-#./script/006.split.pl nuc/DPB_nuc.txt temp/DPB align/DPB.tsv
-#./script/006.split.pl nuc/DQB_nuc.txt temp/DQB align/DQB.tsv
-#./script/006.split.pl nuc/DRB_nuc.txt temp/DRB align/DRB.tsv
-#./script/006.split.pl nuc/classI_nuc.txt temp/classI align/classI.tsv
 
-perl -ne 'print unless m/^#/' ./script/hg38.hla_region-IMGT.bed | perl -lane '$F[1]-=1000; $F[2]+=1000; print join("\t", @F)' > hla.bed
+cat align/DQB.tsv align/DRB.tsv align/DPB.tsv align/ClassI.tsv dqb2/DQB2.tsv > hla.tsv
+cat exon/*.shift > hla.shift
+cat exon/*.faa dqb2/DQB2.faa > hla.faa
+#diamond index hla.faa to hla.dmnd based on the DIAMOND version you are using
